f01e56db81728e6d5b653e34f843485820114f6d
Yusuke-Motohashi <67465288+Yusuke-Motohashi@users.noreply.github.com>
2020-06-28 20:35:06 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php
+    include('_header.php')
+?>
+
+    <div class="main">
+        <div class="contents">
+            <h3>商談テンプレート</h3>
+            <ul class="agenda">
+                <li><a href="#1">会社概要</a></li>
+                <li><a href="#2">訪問経緯</a></li>
+                <li><a href="#3">アジェンダ</a></li>
+                <li><a href="#4">サービス説明</a></li>
+                <li><a href="#5">ヒアリング</a></li>
+                <li><a href="#6">サービス提案</a></li>
+                <li><a href="#7">料金表</a></li>
+                <li><a href="#8">クロージング</a></li>
+            </ul>
+        </div>
+        <h4 class="agendas" id="1">会社概要</h4>
+        <div class="grad-wrap">
+            <input id="trigger1" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                    <li>安心感を与えよう</li>
+                    <li>他社と差別化しよう</li>
+                </ul>
+                <img src="company_white_black_kigyou_5dankai.png" alt="company info"><br>
+                世の中には星の数ほど営業会社があり、評判の良くない会社が多いです。<br>
+                「この前こんな営業がいて…」と営業先で愚痴られる事も多いのではないでしょうか。<br>
+                まずは、そんな営業に対する悪いイメージを払拭するため「会社概要」を必ず話しましょう！<br>
+                「○○上場」「本社が○○に」「子会社が○社」これらのステータスが、<br>
+                営業マンの言葉の信憑性を底上げしてくれるはずです。<br><br>
+                既に良く知っていそうな人や、全く興味のなさそうな人には割愛したほうがいいかもしれませんが、<br>
+                個人経営など小規模な営業先に対してはより効果的です。<br><br>
+            </div>
+            <label class="grad-btn" for="trigger1">続きを読む</label>
+        </div>
+        
+        <h4 class="agendas" id="2">訪問理由</h4>
+        <div class="grad-wrap">
+            <input id="trigger2" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                    <li>相手を聞く姿勢にさせよう</li>
+                    <li>クロージングし易くしよう</li>
+                </ul>
+                　繰り返しになりますが、世の中には星の数ほど営業会社があり、<br>
+                クライアントは色々な営業を受けているので、<br>
+                「そのうちの一人」と思われてしまうとどんなに良いサービスでも<br>
+                相手が前のめりにならず、売り辛いです。<br>
+                なので、訪問理由（ストーリー）を考えて「特別感」を出してあげましょう！<br>
+                場所、業種、時期、人、コンセプト…複数の観点を組み合わせて<br>
+                「御社しかない！」と言えるくらいのストーリーを伝えられれば、<br>
+                営業が圧倒的にし易くなるはずです。<br><br>
+                また、訪問理由をしっかり話せていれば、営業としての熱意が伝わり<br>
+                後のクロージングもし易くなります。特に時期に関しての訪問理由を用意しておくと良いでしょう。<br>
+                ただこれに関しても、例外的に結論から言われないとイライラする人や、<br>
+                事業にそこまで思い入れのない人が相手だと余計になる場合もあるため、<br>
+                事前に見極めましょう。<br><br>
+            </div>
+            <label class="grad-btn" for="trigger2">続きを読む</label>
+        </div>
+
+        <h4 class="agendas" id="3">アジェンダ</h4>
+        <div class="grad-wrap">
+            <input id="trigger3" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                    <li>話を逸れにくくしよう</li>
+                    <li>念のため、再訪を切り易くしよう</li>
+                </ul>
+                　会話をしているうちに相手の話が長くなったり、<br>
+                ちょっと関係ない質問をしてきて本題から逸れる事もあるでしょう。<br>
+                盛り上がるのは良いですが、あまりにも逸れてる時間が長いと危険です。<br>
+                予め今日話さなくてはいけない内容（アジェンダ）を伝えておき、レールを敷いておきましょう。<br>
+                もしそれでも予想外に時間が延びてしまった場合でもアジェンダを伝えておけば、<br>
+                「まだ○○についてお話しできていないので、もう一度お時間ください」と<br>
+                営業が途中である事を理解してもらいやすく、再訪が切り易くなります。<br><br>
+            </div>
+            <label class="grad-btn" for="trigger3">続きを読む</label>
+        </div>
+
+        <h4 class="agendas" id="4">サービス説明</h4>
+        <div class="grad-wrap">
+            <input id="trigger4" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                    <li>まずサービスを理解してもらおう</li>
+                </ul>
+                <img src="tablet_setsumei_business_woman.png" alt="explain"><br>
+                　営業先に行き話し始めると、「あ〜こういう感じね」のようなリアクションを取られる事がたまにあると思います。<br>
+                アポイントの段階では最低限の話しかしていない事が多いので、<br>
+                そもそもどのような類のサービスかをまず理解してもらった方がいいでしょう。<br>
+                後の「サービス提案」とは異なり、まずは<br>
+                ・一般的にどのように使われているモノなのか<br>
+                ・どのようなメリットがあるモノなのか<br>
+                ・似ているモノが何で、それとの違いはなんなのか<br>
+                など、営業先のリテラシーに合わせて端的に伝えましょう。<br>
+                ここは例え話があると分かりやすいと思います。<br><br>
+            </div>
+            <label class="grad-btn" for="trigger4">続きを読む</label>
+        </div> 
+            
+            <h4 class="agendas" id="5">ヒアリング</h4>
+            <div class="grad-wrap">
+                <input id="trigger5" class="grad-trigger" type="checkbox">
+                <div class="grad-item">
+                    <ul class="point">
+                        <li>課題・需要を聞き出そう</li>
+                        <li>それを営業先自身にも認識させよう</li>            
+                    </ul>
+                    <div class="text">
+                        人はモノを買う時、それが必要だから買います。<br>
+                        例えば、広告を出す時は「広告を出したいから」ではなく、<br>
+                        「集客したいから」「認知度を上げたいから」などという需要があり、裏返すと<br>
+                        「今満足な集客が出来ていないから」「充分に認知されていないから」<br>
+                        という課題に繋がります。<br><br>
+                        これを聞き出すのがヒアリングです。<br>
+                        ヒアリングをしないと次の「サービス提案」に移れず、サービス説明から先に進めません。<br><br>
+                        また、この課題・需要がなんとなく事前にわかっている場合であっても、<br>
+                        それを営業先自身の口から言わせ、再認識させる事も重要です。<br>
+                        そうでないと「良く考えたらそこまで今困ってないし」と始まってしまいます。<br><br>
+                    </div>
+                </div>
+                <label class="grad-btn" for="trigger5">続きを読む</label>
+            </div>    
+
+
+        <h4 class="agendas" id="6">サービス提案</h4>
+        <div class="grad-wrap">
+                <input id="trigger6" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                    <li>課題解決できるイメージをつけよう</li>
+                </ul>
+                    　ヒアリングで得た課題に対して、サービスがどのように有効かを伝えて行きましょう。<br>
+                    似た事例が既にあれば、それを参考に話していくのが良いでしょう。<br>
+            </div>
+            <label class="grad-btn" for="trigger6">続きを読む</label>
+        </div>
+
+
+
+        <h4 class="agendas" id="7">料金表</h4>
+        <div class="grad-wrap">
+            <input id="trigger7" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                <li>営業先が気になったタイミングで見せよう</li>
+                <li>安く感じさせよう</li>
+                </ul>
+                　料金表に関しては、営業先が気になったタイミングで見せてあげるのがいいでしょう。<br>
+                料金が気になるという事は少なからず「あった方がいいな」という気持ちが高まっている証拠です。<br>
+                勿体ぶると印象があまり良くないので、相手が気になってきていそうなタイミングで素直に見せてあげましょう。<br><br>
+            </div>
+            <label class="grad-btn" for="trigger7">続きを読む</label>
+        </div>
+
+        <h4 class="agendas" id="8">クロージング</h4>
+        <div class="grad-wrap">
+            <input id="trigger8" class="grad-trigger" type="checkbox">
+            <div class="grad-item">
+                <ul class="point">
+                <li>課題を再再認識させよう</li>
+                <li>訪問理由を盾にしよう</li>
+                </ul>
+                <img src="dentaku_businesswoman.png" alt="explain"><br>
+                　いざモノを買うとなると、人は料金だけに目が行きがちです。<br>
+                出費ではなく、課題を解決するための投資である事を再度認識させましょう。<br>
+                また、クロージングは営業感が出てしまいやすく、せっかくサービスは気に入っていたのに冷めてしまった。という事もあるので、
+                訪問理由も再度伝え、ただ売りたいだけではない事が伝わるようにしましょう。<br>
+            </div>
+            <label class="grad-btn" for="trigger8">続きを読む</label>
+        </div>
+ 
+        <p><a class="button" href="#1">Topへ</a></p>
+
+    </div>
+
+
+
+    <?php
+    include('_footer.php')
+    ?>
